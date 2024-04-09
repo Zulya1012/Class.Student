a40058abf193418482f4b9216a991b7ba9d3b37e
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-09 16:54:31 +0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,36 @@
+class Student{
+  private String name;
+  private int age;
+  private String email;
+  
+  void print(){
+   
+   System.out.println(name + " " +age + " "+ email);
+  }
+  void SetEmail(String email) throws Exception{
+
+   if (email.equals("")){
+     throw new Exception("Email important");
+   }
+   else{
+     this.email=email;
+   }
+  }
+  void SetName (String name) throws Exception{
+    if (name.length() > 10){
+      throw new Exception("Name cannot more 10 simvol");
+    }
+    else{
+      this.name=name;
+    }
+  }
+  void SetAge (int age) throws Exception{
+    if (age > 100 || age < 10 ){
+      throw new Exception("Age cannot more 100 and less than 10");
+    }
+    else{
+      this.age=age;
+    }
+    
+  }
+} 
---
+++ d2.java
@@ -0,0 +1,20 @@
+public class d2{
+  public static void main (String[] arg) throws Exception{
+    
+    Student student1=new Student();
+    Student student2=new Student();
+    student1.SetName("Vasif");
+    student1.SetEmail("vasif@gmail.com"); 
+    student1.SetAge(15);
+    student2.SetName("Tahir");
+    student2.SetEmail("tahir@gmail.com");
+    student2.SetAge(20);
+    
+ 
+      student1.print();
+    
+      student2.print();
+    
+}
+} 
+
